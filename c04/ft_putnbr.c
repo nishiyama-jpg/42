af3d05bae13d23fb480e9ad067e4e9e7b4f34b11
TheModuloProject <themoduloproject@hotmail.com>
2026-06-25 15:47:52 +0400

    more exercisess

--- c04/ft_putnbr.c
+++ c04/ft_putnbr.c
@@ -17,6 +17,11 @@ void ft_putnbr(int nb)
         ft_putchar('-');
         nb = -nb;
     }
+    if (nb >= 0 && nb <= 9)
+    {
+		ft_putchar(nb + '0');
+    }
+
     if (nb >= 10)
         ft_putnbr(nb / 10);
     ft_putchar((nb % 10) + '0');
